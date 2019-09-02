9a3e294ebe114a7da295e7e3d94ec405de1c35f3
yeji-dev <yeji.dev@gmail.com>
2019-09-03 06:31:41 +0900

    output success

--- README.md
+++ README.md
@@ -1,40 +1,54 @@
 # prography_5th_dl
 프로그라피 5기 지원자 홍예지
-###Summary of assiginment
-<ul>
-    <li>Mission : 냉장고 속 물체 탐지(Object Detection)</li>
-    <li>Framework : Keras</li>
-    <li>Detection Network : <a href="https://github.com/matterport/Mask_RCNN">Mask RCNN</a> (Two-Stage Method)</li>
-    <li>Structure
-        <ol>
-        <li>Multi Classification Model (CNN)</li>
-        <li>Binary Classification Model (이미지에 객체있는지 분류)</li>
-        <li>Regression Model (박스 그려주는 모델)</li>
-        </ol>
-    </li>
-</ul>
 
-###Result
-입력
+##Summary of assiginment
++ Project : 냉장고 속 물체 탐지(Object Detection in refrigerator)
++ Framework : `Keras`
++ Detection Network : `Mask RCNN`
++ Network Structure
+    1. Multi Classification Model (CNN)
+    2. Binary Classification Model (이미지에 객체있는지 분류)
+    3. Regression Model (박스 그려주는 모델)
 
-    python test.py --model [model name] --image [input image]
-
-수행 결과
+##Result
++ Input
+   
+   - `python test.py --model mask_rcnn_refri_cfg_0030.h5 --image apple_0068.jpg`
     
-    어쩌구저쩌구
+   - or `python test.py --image apple_0068.jpg`
 
-<ul>
-    <li>Accuracy : </li>
-</ul>
++ Result
+    - Object detection result
+    ![result_predict](./readme/result_predict.png "Object detection")
+    
+    - Results when inserting unrelated pictures
+    ![result_unrelated](./readme/result_unrelated.png "Results when inserting unrelated pictures")
+    
+* Note: command line options
+![help](./readme/cmdline_help.png "Show help option in the command line")
 
-<hr>
-
-###Description of Code
-<ul>model.h5
-    <ul>d</ul>
-    <ul>d</ul>
-</ul>
-<ul>csv_to_xml.py
-    <ul>Transfer csv to xml file</ul>
-    <ul>bbox에 사용할 절대좌표로 만들어 줌 (w*상대값, h*상대값)</ul>
-</ul>
+##Implementations
++ `mask_rcnn_refri_cfg_0030.h5`
+    - this model has learned `detection_dataset` in the existing `mask_rcnn_coco.h5` model
+    - training description
+        - **Accurcy** : 
+        - data size
+            - train size : 1341, validation size : 169
+        - batch_size : 1341, epoch : 30
+        - learning duration : 1d
++ `test.py`
+    - training and testing in 한파일에서 한번에된다. 맨위의 MODE 변수에 True/False값 넣어서 제어
+    - **Accurcy**
+        - find object(binary classification) : 
+        - bbox position(regression) : 
+    - added custom func to `mrcnn/visualize.py`
+    - handle command line args
++ `csv_to_xml.py`
+    - make an absolute coordinate for use in the bbox
+    - convert .csv to .xml and create new files at each `refri_dataset/.../annots/` folder
+    - `test.py`의 main에서 csv_to_xml()로 사용가능
+    
+###References
+- [Mask RCNN Project, GitHub.](https://github.com/matterport/Mask_RCNN)
+- [Mask R-CNN, 2017, Paper.](https://arxiv.org/abs/1703.06870)
+- [How to Train an Object Detection Model with Keras, Blog.](https://machinelearningmastery.com/how-to-train-an-object-detection-model-with-keras)
--- test.py
+++ test.py
@@ -24,15 +24,15 @@ from mrcnn.visualize import my_display_instances
 import argparse
 
 # select mode
-TRAIN_MODE = True
-TEST_MODE = False
+TRAIN_MODE = False
+TEST_MODE = True
 
 # class that defines and loads the refrigerator dataset
 class RefriDataset(Dataset):
     # load the dataset definitions
-    def load_dataset(self, dataset_dir, is_train):
-        if is_train is False and dataset_dir[0:6] is not 'refri_':
-            # self.add_class("dataset", 1, "guess_what")
+    def load_dataset(self, dataset_dir, is_train=True):
+        # guess what the new image
+        if dataset_dir[0:13] != 'refri_dataset':
             self.add_class("dataset", 1, "apple")
             self.add_class("dataset", 2, "beer")
             self.add_class("dataset", 3, "egg")
@@ -40,6 +40,8 @@ class RefriDataset(Dataset):
             self.add_class("dataset", 5, "milk")
             self.add_class("dataset", 6, "soju")
             self.add_image('dataset', image_id=0, path=dataset_dir, annotation='none_annots.xml')
+
+        # images and annots
         else:
             # define classes
             self.add_class("dataset", 1, "apple")
@@ -206,8 +208,7 @@ def plot_actual_vs_predicted(dataset, model, cfg, n_images=5):
 def plot_test(img_url, model, cfg):
     # load image (only one for cmd testing)
     image = img_url.load_image(0)
-    #Expects an RGB image (or array of images) and subtracts
-    #the mean pixel and converts it to float. Expects image colors in RGB order.
+    # expects an RGB image (or array of images) and subtracts
     scaled_image = image.astype(np.float32) - np.array([123.7, 116.8, 103.9])
     sample = expand_dims(scaled_image, 0)
 
@@ -223,20 +224,23 @@ def plot_test(img_url, model, cfg):
     class_names.insert(6, "soju")
 
     # show the figure
+    my_display_instances(image, yhat['rois'], yhat['class_ids'], class_names) # without masks pyplot
     # display_instances(image, yhat['rois'], yhat['masks'], yhat['class_ids'], class_names)
-    my_display_instances(image, yhat['rois'], yhat['class_ids'], class_names)
 
 
 def main():
+    '''
     # handle command line args
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', type=str, metavar='[model name]', default='mask_rcnn_refri_cfg_0005.h5', help="Please input the model name. and you can also omit model options.")
-    parser.add_argument('--image', type=str, metavar='[input image]', help="Please input the image name.")
+    parser.add_argument('--model', type=str, metavar='[model name]', default='mask_rcnn_refri_cfg_0030.h5',
+                        help="Please input the model name. and you can also omit model options.")
+    parser.add_argument('--image', type=str, metavar='[input image]',
+                        help="Please input the image name.")
 
     args = parser.parse_args()
     model_url = args.model
     image_url = args.image
-
+    '''
 
     # load the train dataset
     train_set = RefriDataset()
@@ -245,7 +249,7 @@ def main():
     print('Train: %d' % len(train_set.image_ids))
     # load the validation dataset
     valid_set = RefriDataset()
-    valid_set.load_dataset('refri_dataset/validation', is_train=False)
+    valid_set.load_dataset('refri_dataset/validation', is_train=True)
     valid_set.prepare()
     print('Validation: %d' % len(valid_set.image_ids))
     # load the test dataset
@@ -254,10 +258,9 @@ def main():
     test_set.prepare()
     print('Test: %d' % len(test_set.image_ids))
 
-
     if TRAIN_MODE:
-        # trans csv to xml
-        csv_to_xml(True)
+        # convert csv to xml
+        csv_to_xml(False)
         '''
         # check train data
         # define image id
@@ -280,28 +283,35 @@ def main():
         model.load_weights('mask_rcnn_coco.h5', by_name=True,
                            exclude=["mrcnn_class_logits", "mrcnn_bbox_fc", "mrcnn_bbox", "mrcnn_mask"])
         # train weights (output layers or 'heads')
-        model.train(train_set, valid_set, learning_rate=cfg1.LEARNING_RATE, epochs=20, layers='heads')
+        model.train(train_set, valid_set, learning_rate=cfg1.LEARNING_RATE, epochs=30, layers='heads')
 
     if TEST_MODE:
+
         # create config
         cfg2 = PredictionConfig()
         # define the model (inference)
         model = MaskRCNN(mode='inference', model_dir='./', config=cfg2)
         # load model weights
-        model_path = 'refri_cfg20190901T1649/'
+        model_path = 'refri_cfg20190902T0621/'
+        ####
+        model_url = 'mask_rcnn_refri_cfg_0030.h5'
+        ####
         model.load_weights(model_path + model_url, by_name=True)
-        '''
+
+
         # evaluate model on training dataset
         train_mAP = evaluate_model(train_set, model, cfg2)
         print("Train mAP: %.3f" % train_mAP)
         # evaluate model on test dataset
-        test_mAP = evaluate_model(test_set, model, cfg2)
+        test_mAP = evaluate_model(valid_set, model, cfg2)
         print("Test mAP: %.3f" % test_mAP)
-        
+
+
         # plot predictions for train dataset
         plot_actual_vs_predicted(train_set, model, cfg2)
         # plot predictions for test dataset
         plot_actual_vs_predicted(test_set, model, cfg2)
+
         '''
         # load the cmd image
         cmd_image = RefriDataset()
@@ -310,7 +320,7 @@ def main():
         cmd_cfg = RefriConfig()
         # plot test for input image
         plot_test(cmd_image, model, cmd_cfg)
-
+        '''
 
 if __name__=="__main__":
     main()
